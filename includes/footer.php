e7e345728fa9573f2323216b00ceaa0e71bcea4f
ajith4uuu <114105469+ajith4uuu@users.noreply.github.com>
2023-04-04 10:55:47 +0530

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+	</main>
+	<footer>
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+		<script src="https://unpkg.com/@grafana/ui@0.2.0/dist/grafana-ui.umd.js"></script>
+		<script src="https://unpkg.com/@grafana/data@7.2.2/dist/grafana_data.umd.js"></script>
+		<script src="https://unpkg.com/@grafana/toolkit@0.3.0/dist/grafana_toolkit.umd.js"></script>
+		<script src="https://unpkg.com/@grafana/runtime@7.2.2/dist/grafana_runtime.umd.js"></script>
+		<script src="https://unpkg.com/@grafana/toolkit@0.3.0/dist/grafana_toolkit.umd.js"></script>
+		<script src="https://unpkg.com/@grafana/data@7.2.2/dist/grafana_data.umd.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.9.2/d3.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
+		<script src="jira_api.js"></script>
+		<script src="grafana_api.js"></script>
+	</footer>
+</body>
+</html>
