647d22d94001031592f200edc0b1a05bf3f5988f
ajith4uuu <114105469+ajith4uuu@users.noreply.github.com>
2023-04-04 10:56:41 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="index.php">Dashboard</a></li>
+			<li><a href="admin/dashboard.php">Admin Dashboard</a></li>
+			<li><a href="admin/users.php">User Management</a></li>
+			<li><a href="user_panel.php">User Panel</a></li>
+		</ul>
+	</nav>
+	<main>
